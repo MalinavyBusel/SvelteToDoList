1c0277ce84f58086d5c43e78130e059c3c5cabb2
Yahor Bagatyrevich <egorbgtrvch@gmail.com>
2023-03-01 18:05:13 +0300

    added the component for the todolist input

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
-	export let name;
+	import ToDoInputForm from "./ToDoInputForm.svelte";
+
+	let firstName = "John"
+	let lastName = "Doe"
+
+	$: fullname = firstName + " " + lastName 
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<ToDoInputForm userName={fullname}/>
 </main>
 
 <style>
@@ -13,18 +17,5 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
