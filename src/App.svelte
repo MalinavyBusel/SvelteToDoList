d8d08f3fe2ee01a4828ad7b4dd0d5f1151314b75
Yahor Bagatyrevich <egorbgtrvch@gmail.com>
2023-03-01 18:56:26 +0300

    added the interactive tasks list

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,12 @@
 	let lastName = ""
 
 	$: fullname = firstName + " " + lastName 
+
+	function handleSubmit() {
+		if (firstName && lastName) {
+			nameEntered = true
+		}
+	}
 </script>
 
 <main>
@@ -15,14 +21,17 @@
 	<ToDoInputForm userName={fullname}/>
 	<ToDoList />
 {:else}
-	<h1>Welcome to task manager!</h1>
-	<h3>Please enter your name:</h3>
-	<div>
-		<input bind:value={firstName} type="text" placeholder="First name" required>
-	</div>
-	<div>
-		<input bind:value={lastName} type="text" placeholder="Last name" required>
-	</div>
+	<form on:submit|preventDefault={handleSubmit}>
+		<h1>Welcome to task manager!</h1>
+		<h3>Please enter your name:</h3>
+		<div>
+			<input bind:value={firstName} type="text" placeholder="First name" required>
+		</div>
+		<div>
+			<input bind:value={lastName} type="text" placeholder="Last name" required>
+		</div>
+		<button on:click={handleSubmit}>Start managing tasks</button>
+	</form>
 {/if}
 </main>
 
--- src/ToDoList.svelte
+++ src/ToDoList.svelte
@@ -1,6 +1,29 @@
 <script>
-
-
+    let toDoItems = [
+        {text: "Take out trash", status: true}, 
+        {text: "Wash the dishes", status: true},
+        {text: "Clean the table", status: true}
+    ]
+    function removeFromList(i) {
+        toDoItems.splice(i, 1)
+        toDoItems = toDoItems
+    }
 </script>
 
-<h1>ToDoList Component</h1>
+{#each toDoItems as item, index}
+    <div class="toDoItems">
+        <input bind:checked={item.status} type="checkbox">
+        <span class:checked={item.status}>{item.text}</span>
+        <button on:click={() => removeFromList(index)}>Remove</button>
+        <br/>
+    </div>
+{/each}
+
+<style>
+    .checked {
+        text-decoration: line-through;
+    }
+    .toDoItems {
+        text-align: left;
+    }
+</style>
