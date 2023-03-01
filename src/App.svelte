4daf826e8225b232f8b76fecf7f1d5060013940f
Yahor Bagatyrevich <egorbgtrvch@gmail.com>
2023-03-01 18:30:44 +0300

    added the component for todolist and two conditions(logged in and not)

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,29 @@
 <script>
 	import ToDoInputForm from "./ToDoInputForm.svelte";
+	import ToDoList from "./ToDoList.svelte";
+	let nameEntered = false
 
-	let firstName = "John"
-	let lastName = "Doe"
+	let firstName = ""
+	let lastName = ""
 
 	$: fullname = firstName + " " + lastName 
 </script>
 
 <main>
+{#if nameEntered} 
+	<h1>Welcome to task manager, {fullname}</h1>
 	<ToDoInputForm userName={fullname}/>
+	<ToDoList />
+{:else}
+	<h1>Welcome to task manager!</h1>
+	<h3>Please enter your name:</h3>
+	<div>
+		<input bind:value={firstName} type="text" placeholder="First name" required>
+	</div>
+	<div>
+		<input bind:value={lastName} type="text" placeholder="Last name" required>
+	</div>
+{/if}
 </main>
 
 <style>
